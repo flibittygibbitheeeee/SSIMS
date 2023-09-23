7a7d838acf77c048f43cdadc68abe75039a55af3
Greenhills Peligro <peligrogreenhills@gmail.com>
2023-09-23 15:53:02 +0800

    SCUAA responsive navbar.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html> 
+<!-- <!DOCTYPE html> 
 <html lang="en">
 <head>
 <meta charset="UTF-8" />
@@ -20,5 +20,129 @@
             </div>
         </div>
     </div>
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">    
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/mini.png"/>
+    <title>TAU - SSIMS</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/93e3069a39.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="container-fluid">
+                <div class="navb-logo">
+                    <img src="assets/mini.png" alt="hi">
+                    <h2>TAU - SSIMS</h2>
+                </div>
+    
+                <div class="navb-items">
+                    <ul class="main-nav">
+                        <li class="item">
+                            <a href="index.php">HOME</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">ABOUT US</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">CONTACT US</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">SPORTS</a>
+                       </li>
+                        <li class="item">
+                            <a href="#">SOCIOCULTURAL</a>
+                        </li>
+                        <li class="item">
+                            <a href="login.php">LOGIN</a>
+                        </li>
+                    </ul>
+                </div>
+    
+                <!-- Button trigger modal -->
+                <div class="mobile-toggler">
+                    <a href="#" data-bs-toggle="modal" data-bs-target="#navbModal">
+                        <i class="fa-solid fa-bars-staggered"></i>                
+                    </a>
+                </div>
+    
+                <!-- Modal -->
+                <div class="modal fade" id="navbModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><span class="material-icons-outlined">close</span></button>
+                            </div>
+                            <div class="modal-middle">
+                                <img src="assets/mini.png" alt="hi">
+                                <h2>TAU - SSIMS</h2>
+                            <div class="modal-body">
+                                <ul>
+                                    <li id="dashboard">
+                                        <a href="index.php">
+                                            <span class="material-icons-outlined" id="icon">home</span>
+                                            <span class="name">HOME</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li id="dashboard">
+                                        <a href="#">
+                                            <span class="material-icons-outlined" id="icon">people</span>
+                                            <span class="name">ABOUT US</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li id="dashboard">
+                                        <a href="#">
+                                            <span class="material-icons-outlined" id="icon">question_answer</span>
+                                            <span class="name">CONTACT US</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li id="dashboard">
+                                        <a href="#">
+                                            <span class="material-icons-outlined" id="icon">laptop</span>
+                                            <span class="name">SPORTS</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li id="dashboard">
+                                        <a href="#">
+                                            <span class="material-icons-outlined" id="icon">question_answer</span>
+                                            <span class="name">SOCIOCULTURAL</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li id="dashboard">
+                                        <a href="#">
+                                            <span class="material-icons-outlined" id="icon">question_answer</span>
+                                            <span class="name">LOGIN</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        
+        <section class="home">
+
+        </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
