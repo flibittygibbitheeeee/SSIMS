75360c5d1fed7b53374a8699e4ddfa6163d3b3fd
flibittygibbitheeeee <peligrogreenhills@gmail.com>
2023-10-12 08:01:41 +0800

    Application form.

---
+++ application_form.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+    <link rel="stylesheet" href="css/form.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">    
+    <title>Form</title>
+</head>
+<body>
+    <div class="content">
+        <form action="" method="POST" enctype="multipart/form-data">
+            <section class="one">
+                <h1>OFFICE OF THE SPORTS AND DEVELOPMENT</h1>
+                <p>VARSITY PLAYER APPLICATION FORM</p>
+            </section>
+            <section class="two" style="margin-top: 0.75rem;">
+                <!-- PICTURE -->
+                <div class="form-input">
+                    <p>Kindly include your 2 x 2 picture <span>*</span></p>
+                    <input type="file" name="files" id="image" accept=".jpg, .jpeg, .png" value=""  />
+                </div>
+                <!-- APPLICATION DATE -->
+                <div class="form-input">
+                    <p>Date of Application <span>*</span></p>
+                    <input type="date" name="app_date" required>
+                </div>
+                <!-- NAME -->
+                <div class="form-input">
+                    <p>Name <span>*</span></p>
+                    <input type="text" name="name" placeholder="Short answer text" required>
+                </div>
+                <!-- SEX -->
+                <div class="form-input">
+                    <p>Sex <span>*</span></p>
+                    <label class="container"><p>Male</p>
+                        <input type="radio" name="sex" value="Male" required><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Female</p>
+                        <input type="radio" name="sex" value="Female"><span class="checkmark"></span>
+                    </label>
+                </div>
+                <!-- AGE -->
+                <div class="form-input">
+                    <p>Age <span>*</span></p>
+                    <input type="number" name="age" placeholder="Short answer text" required>
+                </div>
+                <!-- ADDRESS -->
+                <div class="form-input">
+                    <p>Address <span>*</span></p>
+                    <input type="text" name="address" placeholder="Short answer text" required>
+                </div>
+                <!-- YEAR -->
+                <div class="form-input">
+                    <p>Year Level <span>*</span></p>
+                    <label class="container"><p>First Year</p>
+                        <input type="radio" name="year" value="First Year" required><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Second Year</p>
+                        <input type="radio" name="year" value="Second Year"><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Third Year</p> 
+                        <input type="radio" name="year" value="Third Year"><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Fourth Year</p> 
+                        <input type="radio" name="year" value="Fourth Year"><span class="checkmark"></span>
+                    </label>
+                </div>
+                <!-- COURSE -->
+                <div class="form-input">
+                    <p>Course <span>*</span></p>
+                    <select name="course" id="course" class="select" required>
+                        <option>Choose Below</option>
+                        <option value="BSA (Agricultural Economics)">BSA (Agricultural Economics)</option>
+                        <option value="BSA (Agricultural Extension)">BSA (Agricultural Extension)</option>
+                        <option value="BSA (Agronomy)">BSA (Agronomy)</option>
+                        <option value="BSA (Animal Science)">BSA (Animal Science)</option>
+                        <option value="BSA (Soil Science)">BSA (Soil Science)</option>
+                        <option value="BSA (Crop Protection)">BSA (Crop Protection)</option>
+                        <option value="BAS">Bachelor of Animal Science</option>
+                        <option value="Food-Tech">Bachelor of Science in Food Technology</option>
+                        <option value="Forestry">Bachelor of Science in Forestry</option>
+                        <option value="VetMed">Doctor of Veterinary Medicine</option>
+                        <option value="BSABE">BSABE (Agricultural and Biosystems)</option>
+                        <option value="Geodetic">BSGE (Geodetic)</option>
+                        <option value="BSIT">BSIT (Information and Technology)</option>
+                        <option value="Elem-Educ">Bachelor of Elementary Education</option>
+                        <option value="Early-Educ">Bachelor of Early Childhood Education</option>
+                        <option value="TLE(Home-Econ)">BTLE (Home Economics)</option>
+                        <option value="TLE(ICT)">BTLE (Technology)</option>
+                        <option value="TLE(Agri-Fishery)">BTLE (Agri-Fishery Arts)</option>
+                        <option value="BSE">Bachelor of Secondary Education (Science)</option>
+                        <option value="Cert-HomeTech">Certificate in Home Technology Management</option>
+                        <option value="BSP">Bachelor of Science in Psychology</option>
+                        <option value="Economics">Bachelor of Arts in Economics</option>
+                        <option value="DevCom">BSDC (Developement Communication)</option>
+                        <option value="BSBA(HRM)">BSBA (Human Resource Management)</option>
+                        <option value="BSBA(FM)">BSBA (Final Management)</option>
+                        <option value="BSBA(MM)">BSBA (Marketing Management)</option>
+                        <option value="Entrep">Bachelor of Science in Entrepreneurship</option>
+                        <option value="Tourism">Bachelor of Science in Tourism Management</option>
+                        <option value="AgriBusiness">Bachelor of Science in Agribusiness</option>
+
+                    </select>                
+                </div>
+                <!-- ID Number -->
+                <div class="form-input">
+                    <p>ID Number <span>*</span></p>
+                    <input type="text" name="student_no" placeholder="Short answer text" required>
+                </div>
+                <!-- CONTACT NUMBER -->
+                <div class="form-input">
+                    <p>Contact Number <span>*</span></p>
+                    <input type="text" name="contact_no" placeholder="Short answer text" required>
+                </div>
+                <!-- DATE OF BIRTH -->
+                <div class="form-input">
+                    <p>Date of Birth <span>*</span></p>
+                    <input type="date" name="birthdate" required>
+                </div>
+                <!-- PLACE OF BIRTH -->
+                <div class="form-input">
+                    <p>Place of Birth <span>*</span></p>
+                    <input type="text" name="birthplace" placeholder="Short answer text" required>
+                </div>
+                <!-- MOTHER'S NAME -->
+                <div class="form-input">
+                    <p>Mother's Name <span>*</span></p>
+                    <input type="text" name="mother" placeholder="Short answer text" required>
+                </div>
+                <!-- MOTHER'S CONTACT NUMBER -->
+                <div class="form-input">
+                    <p>Mother's Contact Number <span>*</span></p>
+                    <input type="text" name="mother_no" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Father's Name <span>*</span></p>
+                    <input type="text" name="father" placeholder="Short answer text" required>
+                </div>
+                <!-- FATHER'S CONTACT NUMBER -->
+                <div class="form-input">
+                    <p>Father's Contact Number <span>*</span></p>
+                    <input type="text" name="father_no" placeholder="Short answer text" required>
+                </div>
+                <!-- SPORTS -->
+                <div class="form-input">
+                    <p>Preferred Sports Events <span>*</span></p>
+                    <select name="sports" id="sports" class="select" required>
+                        <option selected="">Choose Below</option>
+                        <option value="athletics">Athletics</option>
+                        <option value="arnis">Arnis</option>
+                        <option value="archery">Archery</option>
+                        <option value="badminton">Badminton</option>
+                        <option value="baseball">Baseball</option>
+                        <option value="basketball">Basketball</option>
+                        <option value="beach volleyball">Beach Volleyball</option>
+                        <option value="boxing">Boxing</option>
+                        <option value="chess">Chess</option>
+                        <option value="dancesports">Dancesports</option>
+                        <option value="football">Football</option>
+                        <option value="futsal">Futsal</option>
+                        <option value="karate-do">Karate-do</option>
+                        <option value="lawn tennis">Lawn Tennis</option>
+                        <option value="pencaksilat">Pencaksilat</option>
+                        <option value="sepak takraw">Sepak Takraw</option>
+                        <option value="softball">Softball</option>
+                        <option value="swimming">Swimming</option>
+                        <option value="table tennis">Table Tennis</option>
+                        <option value="taekwondo">Taekwondo</option>
+                        <option value="volleyball">Volleyball</option>
+                        <option value="billiard">Billiard</option>
+                        <option value="others">Others</option>
+                    </select>                
+                </div>
+                <div class="form-input">
+                    <p>Playing Experience  <span>*</span></p>
+                    <textarea name="experience" class="textarea" required></textarea>
+                </div>
+                <div class="form-input">
+                    <p>Awards Received  <span>*</span></p>
+                    <textarea name="awards" class="textarea" ></textarea>
+                </div>
+            </section>
+            <div class="submit">
+                <button type="submit" name="submit">Submit</button>
+            </div>
+        </form>
+    </div> 
+</body>
+</html>
--- editor.php
+++ editor.php
@@ -1,114 +1,215 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+    <link rel="stylesheet" href="css/form.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">    
+    <title>Form</title>
+</head>
+<body>
+    <div class="content">
+        <form action="" method="POST" enctype="multipart/form-data">
+            <section class="one">
+                <h1>OFFICE OF THE SPORTS AND DEVELOPMENT</h1>
+                <p>VARSITY PLAYER APPLICATION FORM</p>
+            </section>
+            <section class="two" style="margin-top: 0.75rem;">
+                
+            <div class="form-input">
+                    <p>Kindly include your 2 x 2 picture <span>*</span></p>
+                    <input type="file" name="files" id="image" accept=".jpg, .jpeg, .png" value=""  />
+                </div>
+                <!-- COURSE -->
+                <div class="form-input">
+                    <p>Course <span>*</span></p>
+                    <select name="course" id="course" class="select" required>
+                        <option>Choose Below</option>
+                        <option value="BSA (Agricultural Economics)">BSA (Agricultural Economics)</option>
+                        <option value="BSA (Agricultural Extension)">BSA (Agricultural Extension)</option>
+                        <option value="BSA (Agronomy)">BSA (Agronomy)</option>
+                        <option value="BSA (Animal Science)">BSA (Animal Science)</option>
+                        <option value="BSA (Soil Science)">BSA (Soil Science)</option>
+                        <option value="BSA (Crop Protection)">BSA (Crop Protection)</option>
+                        <option value="BAS">Bachelor of Animal Science</option>
+                        <option value="Food-Tech">Bachelor of Science in Food Technology</option>
+                        <option value="Forestry">Bachelor of Science in Forestry</option>
+                        <option value="VetMed">Doctor of Veterinary Medicine</option>
+                        <option value="BSABE">BSABE (Agricultural and Biosystems)</option>
+                        <option value="Geodetic">BSGE (Geodetic)</option>
+                        <option value="BSIT">BSIT (Information and Technology)</option>
+                        <option value="Elem-Educ">Bachelor of Elementary Education</option>
+                        <option value="Early-Educ">Bachelor of Early Childhood Education</option>
+                        <option value="TLE(Home-Econ)">BTLE (Home Economics)</option>
+                        <option value="TLE(ICT)">BTLE (Technology)</option>
+                        <option value="TLE(Agri-Fishery)">BTLE (Agri-Fishery Arts)</option>
+                        <option value="BSE">Bachelor of Secondary Education (Science)</option>
+                        <option value="Cert-HomeTech">Certificate in Home Technology Management</option>
+                        <option value="BSP">Bachelor of Science in Psychology</option>
+                        <option value="Economics">Bachelor of Arts in Economics</option>
+                        <option value="DevCom">BSDC (Developement Communication)</option>
+                        <option value="BSBA(HRM)">BSBA (Human Resource Management)</option>
+                        <option value="BSBA(FM)">BSBA (Final Management)</option>
+                        <option value="BSBA(MM)">BSBA (Marketing Management)</option>
+                        <option value="Entrep">Bachelor of Science in Entrepreneurship</option>
+                        <option value="Tourism">Bachelor of Science in Tourism Management</option>
+                        <option value="AgriBusiness">Bachelor of Science in Agribusiness</option>
 
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="req-form-style.css">
-		<link 
-			rel="stylesheet"
-			href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-			integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-			crossorigin="anonymous"
-		/>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
-		<title>Form</title>
-	</head>
-	
-	<body>
-
-		
-		<!--header area start-->
-		<header>
-			<label for="check" style="display: none;">
-				<i class="fas fa-bars" id="sidebar_btn"></i>
-			</label>
-			
-		</header>
-		
-		<div style="margin-left: 33%; background-color: #F4EBE9; width: 33%;">
-		
-			
-				
-			
-			
-				<form id="forms" method="POST">
-					<h1><p>Assessment Questions</p>
-					    <p>BLA BLA BLA ETC ETC</p>
-					</h1>
-					
-					<span><h4>(1)Rarely</h4></span>
-					<span><h4>(2)Sometimes</h4></span>
-					<span><h4>(3)Most of the time</h4></span>
-					<span><h4>(4)Always</h4></span>
-		
-	
-					
-					<h2>Physical Health</h2>
-						<p>1.) 
-							
-						</p>
-							<input type="hidden" name="c" value="<?php echo $id ?>" required> 
-							<input type="radio" id="1" name="choice1" value="1" required>
-							<label for="1">1</label>
-							<input type="radio" id="2" name="choice1" value="2" required>
-							<label for="2">2</label>
-							<input type="radio" id="3" name="choice1" value="3" required>
-							<label for="3">3</label>
-							<input type="radio" id="4" name="choice1" value="4" required>
-							<label for="4">4</label>
-						<br>
-						<p>2.) 
-							<?php
-						
-				//			$wen = $mysqli->query("SELECT question_physical FROM question_physical_table WHERE question_physical_id = 2") or die($mysqli->error);
-							
-				//			while($row = $wen->fetch_assoc()) 
-				//				{
-				//					echo $row['question_physical'];
-				//				}
-							?>
-						</p>
-							<input type="radio" id="1" name="choice2" value="1" required>
-							<label for="1">1</label>
-							<input type="radio" id="2" name="choice2" value="2" required>
-							<label for="2">2</label>
-							<input type="radio" id="3" name="choice2" value="3" required>
-							<label for="3">3</label>
-							<input type="radio" id="4" name="choice2" value="4" required>
-							<label for="4">4</label>
-						<br>
-						<p>3.) 
-							<?php
-						
-					//		$wen = $mysqli->query("SELECT question_physical FROM question_physical_table WHERE question_physical_id = 3") or die($mysqli->error);
-							
-					//		while($row = $wen->fetch_assoc()) 
-					//			{
-					//				echo $row['question_physical'];
-					//			}
-							?>
-						</p> 
-							<input type="radio" id="1" name="choice3" value="1" required>
-							<label for="1">1</label>
-							<input type="radio" id="2" name="choice3" value="2" required>
-							<label for="2">2</label>
-							<input type="radio" id="3" name="choice3" value="3" required>
-							<label for="3">3</label>
-							<input type="radio" id="4" name="choice3" value="4" required>
-							<label for="4">4</label>
-					
-			
-		</div>
-
-		<script type="text/javascript">
-			$(document).ready(function(){
-				$('.nav_btn').click(function(){
-					$('.mobile_nav_items').toggleClass('active');
-				});
-			});
-		</script>
-
-	</body>
+                    </select>                
+                </div>
+                <div class="form-input">
+                    <p>Year Level <span>*</span></p>
+                    <label class="container"><p>First Year</p>
+                        <input type="radio" name="year" value="First Year" required><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Second Year</p>
+                        <input type="radio" name="year" value="Second Year"><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Third Year</p> 
+                        <input type="radio" name="year" value="Third Year"><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Fourth Year</p> 
+                        <input type="radio" name="year" value="Fourth Year"><span class="checkmark"></span>
+                    </label>
+                </div>
+                <div class="form-input">
+                    <p>Family Name <span>*</span></p>
+                    <input type="text" name="lastname" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Given Name <span>*</span></p>
+                    <input type="text" name="givenname" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Middle Name <span>*</span></p>
+                    <input type="text" name="middlename" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Address <span>*</span></p>
+                    <input type="text" name="address" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Telephone Number <span>*</span></p>
+                    <input type="text" name="telephone" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Cellphone Number <span>*</span></p>
+                    <input type="text" name="cellphone" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Email Address <span>*</span></p>
+                    <input type="email" name="email" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Date of Birth <span>*</span></p>
+                    <input type="date" name="birthdate" required>
+                </div>
+                <div class="form-input">
+                    <p>Sex <span>*</span></p>
+                    <label class="container"><p>Male</p>
+                        <input type="radio" name="sex" value="Male" required><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Female</p>
+                        <input type="radio" name="sex" value="Female"><span class="checkmark"></span>
+                    </label>
+                </div>
+                <div class="form-input">
+                    <p>Civil Status <span>*</span></p>
+                    <label class="container"><p>Single</p>
+                        <input type="radio" name="status" value="single" required><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Married</p>
+                        <input type="radio" name="status" value="married"><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Widowed</p> 
+                        <input type="radio" name="status" value="widowed"><span class="checkmark"></span>
+                    </label>
+                    <label class="container"><p>Divorced</p> 
+                        <input type="radio" name="status" value="divorced"><span class="checkmark"></span>
+                    </label>
+                </div>
+                <div class="form-input">
+                    <p>Religion <span>*</span></p>
+                    <input type="text" name="religion" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Nationality <span>*</span></p>
+                    <input type="text" name="nationality" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Father's Name <span>*</span></p>
+                    <input type="text" name="father" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Mother's Name <span>*</span></p>
+                    <input type="text" name="mother" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Guardian's Name <span>*</span></p>
+                    <input type="text" name="guardian" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Guardian's Cellphone/Telephone Number <span>*</span></p>
+                    <input type="text" name="guardianNo" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Parent's Address <span>*</span></p>
+                    <input type="text" name="parentAddress" placeholder="Short answer text" required>
+                </div>
+                <div class="form-input">
+                    <p>Parent's Cellphone/Telephone Number <span>*</span></p>
+                    <input type="text" name="parentNumber" placeholder="Short answer text" required>
+                </div>
+            </section>
+            <section class="one" style="margin-top: 0.75rem;">
+                <h1>Personal History</h1>
+            </section>
+            <section class="two">
+                <div class="form-input">
+                    <p>Menarche (Edad ng unang regla) <span>*</span></p>
+                    <input type="text" name="menarche" placeholder="Short answer text">
+                </div>
+                <div class="form-input">
+                    <p>Circumcision (Edad ng pagkatuli) <span>*</span></p>
+                    <input type="text" name="circumcise" placeholder="Short answer text">
+                </div>
+            </section> 
+            <div class="submit">
+                <button type="submit" name="submit">Submit</button>
+            </div>
+        </form>
+    </div> 
+</body>
 </html>
+
+<div class="form-input">
+	<p>Preferred Sports Events <span>*</span></p>
+	<select name="sports" id="sports" class="select" required>
+		<option selected="">Choose Below</option>
+		<option value="athletics">Athletics</option>
+		<option value="arnis">Arnis</option>
+		<option value="archery">Archery</option>
+		<option value="badminton">Badminton</option>
+		<option value="baseball">Baseball</option>
+		<option value="basketball">Basketball</option>
+		<option value="beach volleyball">Beach Volleyball</option>
+		<option value="boxing">Boxing</option>
+		<option value="chess">Chess</option>
+		<option value="dancesports">Dancesports</option>
+		<option value="football">Football</option>
+		<option value="futsal">Futsal</option>
+		<option value="karate-do">Karate-do</option>
+		<option value="lawn tennis">Lawn Tennis</option>
+		<option value="pencaksilat">Pencaksilat</option>
+		<option value="sepak takraw">Sepak Takraw</option>
+		<option value="softball">Softball</option>
+		<option value="swimming">Swimming</option>
+		<option value="table tennis">Table Tennis</option>
+		<option value="taekwondo">Taekwondo</option>
+		<option value="volleyball">Volleyball</option>
+		<option value="billiard">Billiard</option>
+		<option value="others">Others</option>
+	</select>                
+</div>
